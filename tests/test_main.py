b9441d65cf09a29fd57f3bd7ba634b1616ca302e
Shrey Gupta <shrey.gupta1904@gmail.com>
2023-07-28 00:42:00 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,17 @@ from main import app
 
 client = TestClient(app)
 
-def test_image_endpoint():
-    response = client.get("/vector_image/panda eating ice cream")
+def test_health_check():
+    response = client.get("/health_check")
     assert response.status_code == 200
-    #assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"msg": "Hello World"}
+
+def test_generate_image():
+    response = client.get("/generate_image/photo of an astronaut riding a horse on mars")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "image/png"
+
+def test_root(self):
+    response = client.get("/")
+    assert response.status_code == 302
+    assert response.headers["location"] == "/docs"
